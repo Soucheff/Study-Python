d4b0c919eb4993acc831af062c4c13944437d1dc
Pedro Soucheff <pedro.soucheff@live.com>
2023-02-18 16:09:21 -0300

    Submited

---
+++ LeetCode/Easy/1-Two Sum.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    index=0
+    i=nums.pop(0)
+    for j in range(i+1,total):
+        if nums[i]+nums[j] == target:
+            return [i,j]
+            
+nums = [3,2,3]
+print(twoSum(nums,6))
---
+++ LeetCode/Easy/2-Add Two Numbers.py
@@ -0,0 +1,58 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def addTwoNumbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    list = ListNode()
+    pl = list
+    dec = 0
+    while True:
+        if l1 == None and l2 == None:
+            break
+        
+        if l1 == None:
+            v1 = 0
+        else:
+            v1 = l1.val
+            l1 = l1.next
+            
+
+        if l2 == None:
+            v2 = 0
+        else:
+            v2 = l2.val
+            l2 = l2.next
+
+        sum = v1+v2+dec
+
+        if sum > 9:
+            sum = sum - 10
+            dec = 1
+        else:
+            dec=0
+
+        pl.val = sum
+
+        if(l1 != None or l2 != None or dec==1) :            
+            pl.next = ListNode()
+        pl = pl.next
+
+    if dec == 1:
+        pl.val = dec
+        dec = 0
+    return list
+
+l1 = ListNode(val=9)
+l1.next = ListNode(val=9)
+
+
+l2 = ListNode(val=9)
+l2.next = ListNode(val=9)
+l2.next.next = ListNode(val=9)
+
+print(addTwoNumbers(l1,l2))
+
